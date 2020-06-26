c77816f35bdabc28ab84796d065550fd68a1a2f3
Christina3107 <christina.knepper@gmail.com>
2020-06-26 15:31:55 +0200

    changes gitignore

---
+++ lib/WIFIconnect.py
@@ -0,0 +1,14 @@
+from network import WLAN
+import keys
+import machine
+
+def connect_to_WIFI():
+    wlan = WLAN(mode=WLAN.STA)
+    wlan.antenna(WLAN.INT_ANT)
+
+    wlan.connect(keys.SSID, auth=(WLAN.WPA2, keys.WIFIkey), timeout=5000)
+
+    while not wlan.isconnected ():
+        machine.idle()
+    print("Connected to Wifi\n")
+    #add green led!!!
