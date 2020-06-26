b7cd148695b61b7fdac62b4ac13cfd062d2f9d8f
Christina3107 <christina.knepper@gmail.com>
2020-06-26 15:30:32 +0200

    WIFI connection established

--- boot.py
+++ boot.py
@@ -1,8 +1,10 @@
 from machine import UART
 import machine
 import os
+import WIFIconnect as conn
 
 uart = UART(0, baudrate=115200)
 os.dupterm(uart)
 
+conn.connect_to_WIFI()
 machine.main('main.py')
