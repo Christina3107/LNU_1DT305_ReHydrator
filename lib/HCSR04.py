0eba7103eb17599a157d962824865ee6ed30703a
Christina3107 <christina.knepper@gmail.com>
2020-07-03 19:02:09 +0200

    minor changes

--- lib/HCSR04.py
+++ lib/HCSR04.py
@@ -1,10 +1,3 @@
-## get_distance
-## get_volume()
-##read previous value
-##set start value
-## add values to array???
-## number of measurements per day: 28? 14 x 2
-
 import utime
 import time
 import pycom
